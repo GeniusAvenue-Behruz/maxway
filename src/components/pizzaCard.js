1e469398886ad96e42e96e0235b9035e046cc44c
Behruz Rajabov <behruzgeniusavenue@gmail.com>
2024-08-14 07:32:15 +0500

    modified:   src/components/pizzaCard.js

--- src/components/pizzaCard.js
+++ src/components/pizzaCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const PizzaCard = ({ bgColor = '#FFC85C', nameColor, width = 'auto', height = 'auto', status = 'Yangi', name = "Pit<br/>&nbsp;-sa", imgSrc, imgAlt = 'Yangi Pizza' }) => {
+const PizzaCard = ({ bgColor = '#FFC85C', nameColor = '#E76A06', width = '255px', height = '200px', status = 'Yangi', name = 'Pit -sa', imgSrc, imgAlt = 'Yangi Pizza' }) => {
   return (
     <div className={`bg-[${bgColor}] w-[${width}] h-[${height}] rounded-xl pt-8 relative overflow-hidden`}>
-      <span className='text-base px-5 py-[10px] bg-[#ffffff69] rounded-2xl ml-5'>{status}</span>
+      <span className='status text-base px-5 py-[10px] bg-[#ffffff69] rounded-2xl ml-5'>{status}</span>
       <div className='flex justify-between items-end absolute bottom-0'>
-      <h1 className={`name pl-8 mb-5 text-[${nameColor}] text-3xl uppercase font-bold`}>
-            {name}
+        <h1 className={`name pl-8 mb-5 text-[${nameColor}] text-3xl uppercase font-bold`}>
+          {name.split('-').map((line, index) => (
+            <span key={index}>{line}<br/>&nbsp;&nbsp;</span>
+          ))}
         </h1>
-        <img className='' src={imgSrc} alt={imgAlt} />
+        <img className='img' src={imgSrc} alt={imgAlt} />
       </div>
     </div>
   );
 };
+
 
 export default PizzaCard;
